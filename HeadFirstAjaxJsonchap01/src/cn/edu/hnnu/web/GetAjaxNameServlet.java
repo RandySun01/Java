072da63e42c779aa5376282611b905f269aa7593
wnqfmqlq <2214644978@qq.com>
2018-11-10 13:50:48 +0800

    Add files via upload

---
+++ HeadFirstAjaxJsonchap01/src/cn/edu/hnnu/web/GetAjaxNameServlet.java
@@ -0,0 +1,36 @@
+package cn.edu.hnnu.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetAjaxNameServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();
+		out.println("波");
+		out.flush();
+		out.close();
+	}
+	
+	
+
+}
